9ba19fd688a6b07bb15799fc5c34c3299accc006
Qirall79 <66278832+Qirall79@users.noreply.github.com>
2020-06-12 19:52:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(".icon-next").click(function () {
+  $(".main-cont1").toggleClass("display");
+  $(".main-cont2").toggleClass("display");
+});
+$(".icon-prev").click(function () {
+  $(".main-cont1").toggleClass("display");
+  $(".main-cont2").toggleClass("display");
+});
